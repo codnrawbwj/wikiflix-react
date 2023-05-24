0a2ee2c1a8671375d365b8f8ab895eed8a9fc408
Chae-uk <codnrawbwj@gmail.com>
2023-05-24 16:43:14 -0500

    OMDb API fetch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
-import React from "react";
+import { useEffect } from 'react';
+
+const myAPI = 'edcc968e';
+const API_URL = 'http://www.omdbapi.com/?apikey=' + myAPI;
+const poster_URL = 'http://img.omdbapi.com/?apikey=' + myAPI;
 
 const App = () => {
+
+    const searchMovie = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search)
+    }
+
+    useEffect(() => {
+        searchMovie('Guardians of the Galaxy');
+    },[])
+
     return (
         <h1>App</h1>
     )
